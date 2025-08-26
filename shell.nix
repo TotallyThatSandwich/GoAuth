b653495e69c2c10b60f9d371efa8e4407e44200a
ThatSandwich <hamishheath2008@gmail.com>
2025-08-26 16:55:41 +1000

    updated nix shell for automatic cloudflared tunnel for dev

--- shell.nix
+++ shell.nix
@@ -2,9 +2,23 @@
 
 pkgs.mkShell {
 
-  buildInputs = [
-    pkgs.go
-    pkgs.sqlc
-  ];
+	buildInputs = [
+    	pkgs.go
+    	pkgs.sqlc
+		pkgs.cloudflared
+  	];
+	shellHook = ''
+    	echo "Starting Cloudflared tunnels..."
+
+    	cloudflared access tcp --hostname pg.genericcursed.com --url localhost:5432 &
+    	PG_TUNNEL_PID=$!
+
+    	cloudflared access tcp --hostname rds.genericcursed.com --url localhost:6379 &
+    	REDIS_TUNNEL_PID=$!
+
+    	export PG_TUNNEL_PID REDIS_TUNNEL_PID
+
+    	trap "kill $PG_TUNNEL_PID $REDIS_TUNNEL_PID" EXIT
+  '';
 
 }
